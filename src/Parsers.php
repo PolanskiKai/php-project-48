7839935f3917095a9e072a2b6b7903290fd08971
Полина Страхова <polinastrahova@MacBook-Pro-Polina.local>
2023-01-15 15:39:02 +0300

    before checking 1.12

--- src/Parsers.php
+++ src/Parsers.php
@@ -9,11 +9,20 @@ function parseFile(string $filePath)
     $format = pathinfo($filePath, PATHINFO_EXTENSION);
     switch ($format) {
         case 'json':
-            $result = file_get_contents($filePath);
-            return json_decode($result, true, 512);
+            return parseJson($filePath);
         case 'yml':
             return Yaml::parseFile($filePath);
         case 'yaml':
             return Yaml::parseFile($filePath);
     }
+}
+
+function parseJson(string $filePath): array
+{
+    $content = file_get_contents($filePath);
+    $decoded = json_decode($content, true);
+    if ($decoded === null) {
+        throw new \Exception("Cannot parse the file\n");
+    }
+    return $decoded;
 }
