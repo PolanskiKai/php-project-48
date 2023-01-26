048d3af3ac2246a32323726b09619194aa77ec0a
Полина Страхова <polinastrahova@MacBook-Pro--Polanski.local>
2023-01-26 22:38:40 +0300

    after review 2

--- tests/DiffTest.php
+++ tests/DiffTest.php
@@ -12,52 +12,61 @@ class DiffTest extends TestCase
     {
         return [
             [
-                'tests/fixtures/resultStylish.txt',
                 'tests/fixtures/file1.json',
-                'tests/fixtures/file2.json',
-                'stylish'
+                'tests/fixtures/file2.json'
             ],
             [
-                'tests/fixtures/resultStylish.txt',
                 'tests/fixtures/file1.yml',
-                'tests/fixtures/file2.yml',
-                'stylish'
-            ],
-            [
-                'tests/fixtures/resultPlain.txt',
-                'tests/fixtures/file1.json',
-                'tests/fixtures/file2.json',
-                'plain'
-            ],
-            [
-                'tests/fixtures/resultPlain.txt',
-                'tests/fixtures/file1.yml',
-                'tests/fixtures/file2.yml',
-                'plain'
-            ],
-            [
-                'tests/fixtures/resultJson.txt',
-                'tests/fixtures/file1.json',
-                'tests/fixtures/file2.json',
-                'json'
-            ],
-            [
-                'tests/fixtures/resultJson.txt',
-                'tests/fixtures/file1.yml',
-                'tests/fixtures/file2.yml',
-                'json'
+                'tests/fixtures/file2.yml'
             ]
         ];
+    }
+    
+    /**
+     * @dataProvider addDataProvider
+     */
+
+    public function testGendiffStylish1($path1, $path2)
+    {
+        $resultPath = 'tests/fixtures/resultStylish.txt';
+        $expectedResult = file_get_contents($resultPath);
+        $result = genDiff($path1, $path2, 'stylish');
+        $this->assertTrue($result == $expectedResult);
     }
 
     /**
      * @dataProvider addDataProvider
      */
 
-    public function testGendiff($resultPath, $path1, $path2, $format)
+    public function testGendiffStylish2($path1, $path2)
     {
+        $resultPath = 'tests/fixtures/resultStylish.txt';
         $expectedResult = file_get_contents($resultPath);
-        $result = genDiff($path1, $path2, $format);
+        $result = genDiff($path1, $path2);
+        $this->assertTrue($result == $expectedResult);
+    }
+
+    /**
+     * @dataProvider addDataProvider
+     */
+
+    public function testGendiffPlain($path1, $path2)
+    {
+        $resultPath = 'tests/fixtures/resultPlain.txt';
+        $expectedResult = file_get_contents($resultPath);
+        $result = genDiff($path1, $path2, 'plain');
+        $this->assertTrue($result == $expectedResult);
+    }
+
+    /**
+     * @dataProvider addDataProvider
+     */
+
+    public function testGendiffJson($path1, $path2)
+    {
+        $resultPath = 'tests/fixtures/resultJson.txt';
+        $expectedResult = file_get_contents($resultPath);
+        $result = genDiff($path1, $path2, 'json');
         $this->assertTrue($result == $expectedResult);
     }
 }
